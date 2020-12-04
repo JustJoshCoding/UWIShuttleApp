6be3f1aa88bd2e7fd0fdb09d39fb88d2cb7b77d5
TriniMisery <36904042+TriniMisery@users.noreply.github.com>
2020-12-04 14:26:40 -0400

    Create Driver_Scan.java
    adding the Driver_Scan.java class

---
+++ Driver_Scan.java
@@ -0,0 +1,165 @@
+package com.project.uwishuttleapp;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.Result;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentChange.Type;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.MetadataChanges;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.Query.Direction;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.ServerTimestamp;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.firestore.Source;
+import com.google.firebase.firestore.Transaction;
+import com.google.firebase.firestore.WriteBatch;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+/*
+The Driver_Scan class is used to implement the functionality of the driver_scanner.xml layout.
+This class is used to implement the scan QR functionality using the camera of the device. The scanner code was found online.
+This class also checks the QR that was scanned to see if it is a valid ticket by comparing it to the information from the online database.
+ */
+public class Driver_Scan extends AppCompatActivity {
+
+    /*
+      class variables
+  */
+    CodeScanner codeScanner;
+    CodeScannerView scannView;
+    TextView resultData;
+    private static final String TAG = "DriverScanner";//this variable is used for error checking information so we know which instance throws the error.
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.driver_scanner);
+
+        /*
+        finding the appropriate ID's and linking them to the variables created for them.
+        begin
+       */
+        scannView = findViewById(R.id.scannerView);
+        codeScanner = new CodeScanner(this,scannView);
+        resultData = findViewById(R.id.resultsOfQr);
+        //end
+
+        /*
+        this code is for when the camera scans tthe QR, to compare it to the Database
+        the data found from the QR after decoded is put through a search of the databse and if it is found the ticket is said to be valid
+        but if not found the ticket will not be valid and hence that student will not be allowed to board shuttle.
+         */
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    //call database search code goes here:
+                    public void run(){
+                        // [START get_document]
+                        db.collection("Tickets")
+                                .whereEqualTo("Ticket Number", result.getText())
+                                .get()
+                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                                resultData.setText("TICKET VALIDATED!");
+                                            }
+                                        } else {
+                                            Log.d(TAG, "Error getting document: ", task.getException());
+                                            resultData.setText("TICKET NOT VALID!");
+                                        }
+                                    }
+                                });
+                        // [END get_document]
+                    }
+
+                });
+
+            }
+        });
+
+        scannView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestForCamera();
+
+    }
+
+    /*
+       this code request for the persion to use the device's camera
+     */
+    public void requestForCamera() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                codeScanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(Driver_Scan.this, "Camera Permission is Required.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                token.continuePermissionRequest();
+
+            }
+        }).check();
+    }
+
+
+}
